12e181cfc9d1a99e553e79928ab8953b82ad7a41
Micaël Dupuis <micaeldupuis@live.fr>
2022-06-02 09:58:19 +0200

    First Commit Exo 1 et 2

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import Compteur from "@/components/Compteur.vue";
+</script>
+<template>
+  <h1>Hello World</h1>
+  <Compteur />
+</template>
+
+<style></style>
---
+++ src/components/Compteur.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref } from "vue";
+
+const compteur = ref(0);
+
+setInterval(() => {
+  compteur.value++;
+}, 1000);
+
+function ResetCompteur(){
+  compteur.value = 0;
+}
+</script>
+
+<template>
+  <p>Compteur : {{ compteur }}</p>
+  <input type="button" @click="ResetCompteur()" value="Reset Compteur" />
+  <input type="number" v-model="compteur" />
+</template>
