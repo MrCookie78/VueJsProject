a51ab60e0c741b6e23c05756476cbf43d8b020e2
Micaël Dupuis <micaeldupuis@live.fr>
2022-06-02 10:20:15 +0200

    Modification taille textarea

--- src/components/JwtDecode.vue
+++ src/components/JwtDecode.vue
@@ -13,13 +13,13 @@ function Decode() {
 <template>
   <div>
     <h2>JWT Encoded</h2>
-    <textarea name="jwt" cols="70" rows="10" v-model="jwt_encoded"></textarea>
+    <textarea name="jwt" cols="70" rows="6" v-model="jwt_encoded"></textarea>
     <br />
     <input type="button" @click="Decode()" value="Decode" />
   </div>
 
   <div>
     <h2>JWT Decoded</h2>
-    {{ claim }}
+    <p>{{ claim }}</p>
   </div>
 </template>
